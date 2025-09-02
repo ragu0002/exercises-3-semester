5e52eb276591bf293da50ac917b928cb5e38809f
Ragnhild <ragu0002@stud.kea.dk>
2025-09-02 13:37:10 +0200

    feat-add-comments

--- arrays/index.js
+++ arrays/index.js
@@ -6,7 +6,7 @@ console.log(letters[4]);
 let newArr = letters;
 newArr[4] = `*`;
 
-console.log(letters[4]);*/
+console.log(letters[4]);
 
 const people = ["Harry", "Ron", "Hermine"];
 let result;
@@ -45,4 +45,31 @@ console.log("result", result);
 
 result = people.slice(result, 1);
 console.log("people", people);
-console.log("result", result);
+console.log("result", result);*/
+
+const animals = [
+  {
+    name: "Mandu",
+    type: "cat",
+  },
+  {
+    name: "Winston",
+    type: "cat",
+  },
+  {
+    name: "Mira",
+    type: "dog",
+  },
+  {
+    name: "Speik",
+    type: "dog",
+  },
+];
+
+function isCat(animals) {
+  if (animals.type === "cat") {
+    return true;
+  } else {
+    return false;
+  }
+}
--- bar_chart_starter_kit/script.js
+++ bar_chart_starter_kit/script.js
@@ -1,6 +1,6 @@
 import { getRandomNum, getQ } from "../utils/utils.js";
 
-const list = document.querySelector("ul");
+const list = getQ("ul");
 const arr = [];
 
 setInterval(getElement, 1000);
@@ -15,7 +15,13 @@ function getElement() {
 
   if (arr.length == 21) {
     arr.shift();
-    document.querySelector("li").remove();
+    getQ("li").remove();
   }
   console.log("sliced");
 }
+
+//i get element fungsjonen blir den oppdatert hvert sekund på grunn av set intervall.
+// For hver gang den blir oppdatert kommer det et nytt random nr inn i arrayet.
+// På samme tid blir det lagt til et nytt li elemtent.
+// Det er på denne måten arrayet vet når det har kommet nr man setter som length i
+// arrayet at den skal fjerne li elementene fra starten!!! ahhhh
