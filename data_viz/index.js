b06b8e7affeb7d6b8b920eaa7feeee0a84d10b2b
Ragnhild <ragu0002@stud.kea.dk>
2025-09-11 10:30:31 +0200

    feat-make-basic-data-viz

---
+++ data_viz/index.js
@@ -0,0 +1,22 @@
+"use strict";
+setInterval(getData, 1000);
+
+async function getData() {
+  const url = "https://kea-alt-del.dk/kata-distortion/";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    console.log(result);
+    showNr(result);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+function showNr(event) {
+  console.log(event.inQueue);
+  document.querySelector("h1").innerText = event.inQueue;
+}
